4bf10d108dd4cdf49f400f81b3363c52bf8b5853
Jesson Yo <Aphostrophy@gmail.com>
2023-02-28 00:22:46 +0700

    added padder for encrypt

--- crypto.py
+++ crypto.py
@@ -6,7 +6,10 @@ class BlockCipher():
         self.key = key
 
     def encrypt(self, plaintext: bytes) -> bytes:
-        pass
+        # Make sure plaintext is a multiple of 16 bytes (128 bits)
+        if len(plaintext) % 16 != 0:
+            padding = b'\00' * (16 - len(plaintext) % 16)
+            plaintext = plaintext + padding
 
     def decrypt(self, ciphertext: bytes) -> bytes:
         pass
--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from crypto import BlockCipher
 if __name__ == "__main__":
     key = b'Sixteen byte key'
     cipher = BlockCipher(key = key)
+    plaintext = b'123456789'
+    cipher.encrypt(plaintext)
