1b64c9ed6b7ec4b40420b27d277cbe4c54be3c77
Frederic Ronaldi <frederic.ronaldi@gmail.com>
2023-03-05 22:24:11 +0700

    add time

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from crypto import BlockCipher
+import time
 
 if __name__ == "__main__":
     key = b'Sixteen byte key'
-    original_plaintext = b'pemilik rumah yang sah bukan anda tetapi ayah ibu anda, jadi jangan bohong lagi!'
+    original_plaintext = b'''Test Plaintext'''
+
+    print('Original Plaintext = ', original_plaintext)
+    print("=============================")
+
+    start_time = time.time()
 
     cipher = BlockCipher(key = key)
     ciphertext = cipher.encrypt(original_plaintext)
-    print('ciphertext', ciphertext)
-    print(len(ciphertext))
 
+    print('Ciphertext = ', ciphertext)
+    print("--- %s seconds ---" % (time.time() - start_time))
     print("=============================")
+
+    start_time = time.time()
 
     cipher = BlockCipher(key = key)
     decrypted_plaintext = cipher.decrypt(ciphertext)
-    print('result', decrypted_plaintext)
-    print(len(decrypted_plaintext))
+    print('Decrypted Plaintext = ', decrypted_plaintext)
+    print("--- %s seconds ---" % (time.time() - start_time))
     
